a5b27993042e4b8862d3ed1387c781de3c10f8fd
Rachel Johnson <2rachy@gmail.com>
2014-12-15 12:15:16 -0700

    Changing HTTP call to a service.

--- game/www/js/controllers.js
+++ game/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $http, $location) {
+.controller('AppCtrl', function($scope, $http, $location, dataSvc) {
 
         // Exit page - Start
         $scope.yes = function (){
@@ -11,65 +11,52 @@ angular.module('starter.controllers', [])
         //Exit page - End
 
     $scope.init = function(){
-
-        $http.get('game_structure/game.json')
-            .success (function(data)
-        {
+        dataSvc.getData(function(data){
             if($scope.questionId == null){
                 //Set questionId
                 $scope.questionId = 1;
             }
-          for(var i=0; i<data.length; i++){
-             if(data[i].id == $scope.questionId){
-                     $scope.data = data[i];
-             }
-          }
+            for(var i=0; i<data.length; i++){
+                if(data[i].id == $scope.questionId){
+                    $scope.data = data[i];
+                }
+            }
+        });
 
-        })
-            .error(function()
-            {
-                alert("error");
-            });
     }
 
         //ClickPicture function
         $scope.clickPicture = function (id) {
-            $http.get('game_structure/game.json')
-                .success (function(data)
-            {
+            dataSvc.getData(function(data){
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].id == id) {
                         $scope.questionId = id;
-                         switch(data[i].template) {
-                             case "1":
-                                 $location.path('app/game_onebutton');
-                                 break;
-                             case "2":
-                                 $location.path('app/game_twobutton');
-                                 break;
-                             case "3":
+                        switch (data[i].template) {
+                            case "1":
+                                $location.path('app/game_onebutton');
+                                break;
+                            case "2":
+                                $location.path('app/game_twobutton');
+                                break;
+                            case "3":
                                 $location.path('app/game_threebutton');
                                 break;
-                             case "4":
+                            case "4":
                                 $location.path('app/game_fourbutton');
                                 break;
-                             case "5":
+                            case "5":
                                 $location.path('app/game_youlose');
-                                 break;
-                             case "6":
-                                 $location.path('app/game_youwin');
                                 break;
-                         }
+                            case "6":
+                                $location.path('app/game_youwin');
+                                break;
+                        }
                         $scope.init();
-                         break;
-
-
+                        break;
                     }
                 }
-            }) .error(function()
-                {
-                    alert("error");
-                });
+            });
+
         }
 
     //Secret Button of Doom
@@ -78,4 +65,16 @@ angular.module('starter.controllers', [])
             $scope.clickPicture(42);
           }
         }
+})
+
+
+.service('dataSvc', function($http){
+
+    this.getData = function(successFunc){
+        var url = 'game_structure/game.json';
+        $http.get(url, null)
+            .success(function(data){
+                successFunc(data);
+            });
+    };
 });
